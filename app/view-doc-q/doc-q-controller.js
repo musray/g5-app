1d1a00d2141738a02cf5e4539fae206a47a0108c
unknown <churui@ctecdcs.net>
2015-11-27 15:50:43 +0800

    first commit for doc query

--- app/view-doc-q/doc-q-controller.js
+++ app/view-doc-q/doc-q-controller.js
@@ -5,7 +5,40 @@ angular.module('g5App.docApp', ['ngRoute'])
       controller: 'DocQuery as docq'
     })
   }])
-  .controller('DocQuery', [function() {
+  .controller('DocQuery', ['$http', function($http) {
     var self = this;
     self.message = "Page for document query. Under construction";
+
+    $http.get('./view-doc-q/doc_list.json').success(function(data) { self.items = data; });
+
+    // self.referedDocs = [
+    //   'BDSD', 
+    //   'Arrangement', 
+    //   'Assembly', 
+    //   'AT Software', 
+    //   'AT Test Table',
+    //   'ATWS', 
+    //   'Cable', 
+    //   'card setting',
+    //   'Equipment List',
+    //   'Equipment Spec',
+    //   'External Cable',
+    //   'Hardwired Logic',
+    //   'Installation Layout',
+    //   'Internal Cable',
+    //   'IO List',
+    //   'n.a',
+    //   'Software',
+    //   'Software Diagram',
+    //   'Terminal Block',
+    //   'WD(IF)'
+    // ];
+
+    self.referedDocs = {
+      '': '',  
+      'BDSD': {'refer_to':'BDSD'},
+      'Arrangement': {'refer_to':'Arrangement'}
+    };
+    self.hardCodeFilter = {'refer_to': 'BDSD'};
+
 }])

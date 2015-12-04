623705d68df5ca84c28e200a156db67274cc6726
musray <churui.ctec@gmail.com>
2015-12-04 15:18:10 +0800

    add ng-class for active nav bar

--- app/app.js
+++ app/app.js
@@ -7,3 +7,10 @@ angular.module('g5App', [
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/'});
   }])
+
+  .controller('HeadController', ['$location', function($location){
+    var self = this;
+    self.isActive = function (viewLocation){
+      return viewLocation === $location.path();
+    };
+  }])

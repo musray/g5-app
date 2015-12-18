0d8f04a230351665d89271f28cec7cd115e4eb5d
musray <churui.ctec@gmail.com>
2015-12-18 09:08:57 +0800

    update view: dcs component

--- app/view-dcs-component/dcs-comp-controller.js
+++ app/view-dcs-component/dcs-comp-controller.js
@@ -1,11 +1,11 @@
 angular.module('g5App.DCSComponent', ['ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/dcs_component', {
-      templateUrl: 'view-DCSComponent/dcscomp.html',
+      templateUrl: 'view-dcs-component/dcscomp.html',
       controller: 'DCSCompCtrl as compCtrl'
     })
   }])
   .controller('DCSCompCtrl', ['$http', function($http) {
     var self = this;
-    $http.get('./view-DCSComponent/dcs-comp-list.json').success(function(data) { self.items = data; });
+    $http.get('./view-dcs-component/dcs-comp-list.json').success(function(data) { self.items = data; });
   }])
